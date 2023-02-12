b19af6fd510791a8ca42f7cef4dc0ace873bb12b
Moataz <elkingmizoman@gmail.com>
2023-02-12 16:42:43 +0200

    added contorller for the ball (player)

--- game.py
+++ game.py
@@ -6,36 +6,34 @@ pygame.init()
 
 size = width, height = 600, 600
 
-speed = [3, 2]
-
 black = 0, 0, 0
 
-
 screen = pygame.display.set_mode(size)
-
 ball = pygame.image.load("intro_ball.gif")
 
 ballrect = ball.get_rect()
 print(ballrect)
-
+ballXpos = ballrect.left
+ballYpos = ballrect.top
 
 while True:
-    for event in pygame.event.get():
+    ballrect.left = ballXpos
+    ballrect.top = ballYpos
+    pygame.key.set_repeat(10)
 
+    for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    ballrect = ballrect.move(speed)
-
-    if ballrect.left < 0 or ballrect.right > width:
-
-        speed[0] = -speed[0]
-
-    if ballrect.top < 0 or ballrect.bottom > height:
-
-        speed[1] = -speed[1]
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                ballXpos -= 1
+            if event.key == pygame.K_d:
+                ballXpos += 1
+            if event.key == pygame.K_w:
+                ballYpos -= 1
+            if event.key == pygame.K_s:
+                ballYpos += 1
 
     screen.fill(black)
-
     screen.blit(ball, ballrect)
-
     pygame.display.flip()
